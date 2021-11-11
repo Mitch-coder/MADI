2b8ade6ce64f52bb9045f6179f65ce550cb947e0
Gabriela Robleto <magabriela0715@gmail.com>
2021-11-10 23:16:01 -0600

    Cambios en documentacion agregados

---
+++ frontend/src/components/documentation/Cuantificadores.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cuantificadores() {
+    return (
+        <div>
+            <h1> Cuantificadores </h1>
+        </div>
+    )
+}
+
+export default Cuantificadores
