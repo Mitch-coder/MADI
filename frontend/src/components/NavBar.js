c26cc92ebe05597660e60418b72e3101d6ba453d
Jurgen <jurgenfbermudez@gmail.com>
2021-11-29 10:18:57 -0600

    Home

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -22,7 +22,7 @@ export default class NavBar extends Component {
         <div>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-              <Navbar.Brand href="#home">Madi</Navbar.Brand>
+              <Navbar.Brand href="/">Madi</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
